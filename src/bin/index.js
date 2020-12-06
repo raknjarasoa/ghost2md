21431801bc0dfd0d2fe6aa15a619998a7936a6f2
Njarasoa Rakotozafy <raknjarasoa@gmail.com>
2020-12-06 20:38:51 +0100

    try again

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
-const fetchContent = require('../dist').fetchContent;
+const fetchContent = require('../../dist').fetchContent;
 
 const apiUrl = process.env.GHOST_API_URL;
 const contentApiKey = process.env.GHOST_CONTENT_API_KEY;
